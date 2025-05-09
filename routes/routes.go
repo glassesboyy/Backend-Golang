18bfd587cfcf2f187e2c843398ee2e0dde5c01e0
glassesboyy <aliperwira26@gmail.com>
2025-05-10 05:41:54 +0700

    buat restful api untuk login, register, dan get all data user

---
+++ routes/routes.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"golang/backend-api/controllers"
+	"golang/backend-api/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+
+	//initialize gin
+	router := gin.Default()
+
+	// route register
+	router.POST("/api/register", controllers.Register)
+
+	// route login
+	router.POST("/api/login", controllers.Login)
+
+	// route users
+	router.GET("/api/users", middlewares.AuthMiddleware(), controllers.FindUsers)
+
+	return router
+}
--- structs/user.go
+++ structs/user.go
@@ -32,4 +32,3 @@ type UserLoginRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
-}
